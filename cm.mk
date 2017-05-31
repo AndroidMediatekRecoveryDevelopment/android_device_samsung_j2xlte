d88cddfecc4fd8a081f7314cf0eb3ae43ec3dae9
adi766 <adityakumarteli7846@gmail.com>
2017-05-31 21:33:00 +0530

    Initial commit

---
+++ cm.mk
@@ -0,0 +1,15 @@
+# Release name
+PRODUCT_RELEASE_NAME := j2xlte
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/j2xlte/device_j2xlte.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := j2xlte
+PRODUCT_NAME := cm_j2xlte
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := j2xlte
+PRODUCT_MANUFACTURER := samsung
---
+++ vendorsetup.sh
@@ -0,0 +1,1 @@
+add_lunch_combo cm_j2xlte-eng
